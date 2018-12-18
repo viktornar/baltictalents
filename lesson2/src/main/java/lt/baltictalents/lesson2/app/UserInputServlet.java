0092b17a9901a0fdeb528ff6c97bcaa50b7a4ed1
Viktor Nareiko <viktor.nareiko@adaptagency.com>
2018-12-18 02:33:51 +0200

    Removed unused import.

--- lesson2/src/main/java/lt/baltictalents/lesson2/app/UserInputServlet.java
+++ lesson2/src/main/java/lt/baltictalents/lesson2/app/UserInputServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.logging.Logger;
 

1600e9fa956b880bc08a2b3d2dc41a7343393827
Viktor Nareiko <vnareiko.lt@gmail.com>
2020-06-25 19:56:56 +0300

    Added forms for editing petclinic

--- petclinic/src/main/java/com/sd/petclinic/owners/OwnerController.java
+++ petclinic/src/main/java/com/sd/petclinic/owners/OwnerController.java
@@ -1,5 +1,8 @@
 package com.sd.petclinic.owners;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +13,17 @@ public class OwnerController {
     OwnersRepository ownersRepository;
 
     @GetMapping("/owners")
-    String showOwners() {
+    String showOwners(Map<String, Object> model) {
+        List<Owner> owners = ownersRepository.findAll();
+        // It is possible to use just plane Map<String, Object> or ModelAndView
+        model.put("owners", owners);
         return "owners/list";
+    }
+
+    @GetMapping("/owners/new")
+    String createOwner(Map<String, Object> model) {
+        Owner owner = new Owner();
+        model.put("owner", owner);
+        return "owners/createOrUpdate";
     }
 }
